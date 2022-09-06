37e9ef424e456e5f42faf42f150c35ab85782f29
Jeevan Banga <94250003+JeevanBanga@users.noreply.github.com>
2022-09-06 10:51:06 -0700

    Project Commit
    Java Tree project located in the src folder

---
+++ src/Tree.java
@@ -0,0 +1,15 @@
+// Define your tree class in this file!
+public class Tree {
+    public static void main(String[] args) {
+        // We’re going to introduce ourselves and print a tree to the screen.
+        System.out.println("Hey there, I'm Jeevan and I'm learning to code in Java!");
+        System.out.println("For my first project, I am going to plant a tree! Take a look below:");
+        System.out.println(" ");
+        System.out.println("*****");
+        System.out.println("*****");
+        System.out.println("*****");
+        System.out.println("  *  ");
+        System.out.println("  *  ");
+        System.out.println("#####");
+    }
+}
